69326988a8de9073ff46faa69156e79ab8fe3f98
Kunjan Dalal <kunjee17@gmail.com>
2018-08-10 12:52:48 +0530

    hotels and flight service added with fake data

---
+++ src/app/api/hotels.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import * as faker from 'faker';
+import * as _ from 'lodash';
+
+const fakeData = 20;
+
+type HotelDTO = {
+  Name : string
+  Image : string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HotelsService {
+
+  constructor() { }
+
+  GetHotelName() {
+    let hotels : HotelDTO [] = [];
+
+    for (let i = 0; i < fakeData; i++) {
+      hotels.push({
+        Name : faker.company.companyName(),
+        Image : "https://loremflickr.com/640/460/hotel"
+      })
+    }
+    
+    return hotels;
+  }
+
+}
