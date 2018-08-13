71089b865935e760edcbfc8036642f6f9fedb56a
Ami Bhatt <bhattami21@gmail.com>
2018-08-13 17:35:55 +0530

    hotels page UI for checking

--- src/app/api/hotels.service.ts
+++ src/app/api/hotels.service.ts
@@ -7,6 +7,11 @@ const fakeData = 20;
 export type HotelDTO = {
   Name : string
   Image : string
+  Addresscity:string
+  Addressstate:string
+  Addresscountry:string
+  Checkindate:Date
+  Checkoutdate:Date
 }
 
 @Injectable({
@@ -18,11 +23,16 @@ export class HotelsService {
 
   GetHotelName() {
     let hotels : HotelDTO [] = [];
-
     for (let i = 0; i < fakeData; i++) {
       hotels.push({
         Name : faker.company.companyName(),
-        Image : "https://loremflickr.com/640/460/hotel"
+        Image : "https://loremflickr.com/640/460/hotel",
+        Checkindate:faker.date.future(),
+        Checkoutdate:faker.date.future(),
+        Addresscity:faker.address.city(),
+       Addressstate:faker.address.state(),
+        Addresscountry:faker.address.country(),
+        // Addresszip:faker.address.zipCode()
       })
     }
     
