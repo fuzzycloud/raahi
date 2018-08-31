7a576a6e603c7a7d35f6d2fe381931cceb3c76d5
Ami Bhatt <bhattami21@gmail.com>
2018-08-31 14:23:38 +0530

    basic design of flights search page

---
+++ src/app/flight-search/flight-search.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { FlightSearchPage } from './flight-search.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: FlightSearchPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [FlightSearchPage]
+})
+export class FlightSearchPageModule {}
---
+++ src/app/hotel-detail/hotel-detail.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-detail',
+  templateUrl: './hotel-detail.page.html',
+  styleUrls: ['./hotel-detail.page.scss'],
+})
+export class HotelDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
