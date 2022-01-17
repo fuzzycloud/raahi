bb2a86e21d001ee35185d2d292f92777a14da235
amibhatt <bhattami21@gmail.com>
2022-01-17 17:12:15 +0530

    ng zorro mobile with ionic

--- apps/mobile/src/app/app.module.ts
+++ apps/mobile/src/app/app.module.ts
@@ -6,10 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,NgZorroAntdMobileModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- apps/mobile/src/app/home/home.module.ts
+++ apps/mobile/src/app/home/home.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
 import { HomePageRoutingModule } from './home-routing.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  imports: [CommonModule,NgZorroAntdMobileModule, FormsModule, IonicModule, HomePageRoutingModule],
   declarations: [HomePage],
 })
 export class HomePageModule {}
