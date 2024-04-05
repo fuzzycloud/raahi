44f790c01da521ed011dc36eb128be5d5fdfc093
piyushlalwani <piyush.lalwani@fuzzycloud.in>
2024-04-05 16:04:18 +0530

    Fixes #92 added admin and seller

---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+function App() {
+
+  return (
+    <>
+      <Button variant="contained">Hello admin</Button>
+    </>
+  )
+}
+
+export default App
---
+++ apps/seller/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+
+function App() {
+
+  return (
+    <>
+      <Button variant="contained">Hello seller</Button>
+    </>
+  )
+}
+
+export default App
